833dce94aa157ccbfa9f32c2afe6b8be2cbaccdc
SurendraKumar17 <surendrakumar.pandrinki@gmail.com>
2026-08-23 13:57:17 +0000

    PRD INFRA

--- infrastructure/envs/prod/backend.tf
+++ infrastructure/envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "surendra-terraform-state"
+    bucket         = "surendra-terraform-state1"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
